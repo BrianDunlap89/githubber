04fdb963b01ce829116e11f35c96fa225b4f989d
robertcramer <robertbcramer@icloud.com>
2015-10-14 15:59:42 -0400

    added homework class

--- lib/githubber.rb
+++ lib/githubber.rb
@@ -4,6 +4,7 @@ require "pry"
 require "githubber/version"
 require "githubber/pull_requests"
 require "githubber/issues"
+require "githubber/homework"
 
 module Githubber
   # Your code goes here...
